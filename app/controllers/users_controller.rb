6b788235c6c276568d2a82fd75f72173b2e6277b
EC2 Default User <ec2-user@ip-172-31-45-244.ap-northeast-1.compute.internal>
2024-09-22 22:26:29 +0000

    [Finish] 4-15

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,11 @@
 class UsersController < ApplicationController
+  
   def show
+    @user = User.find(params[:id])
+    @post_images = @user.post_images # 特定のユーザ（@user）に関連付けられた投稿全て（.post_images）を取得し@post_imagesに渡す という処理
   end
 
   def edit
   end
+  
 end
