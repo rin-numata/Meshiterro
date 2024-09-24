6709eede1e2705c23aac71aac6feee2aca0ff6f6
EC2 Default User <ec2-user@ip-172-31-45-244.ap-northeast-1.compute.internal>
2024-09-24 05:17:22 +0000

    [Finish] 4-22

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -13,7 +13,7 @@ class PostImagesController < ApplicationController
       render :new
     end
   end
-  
+
   def destroy
     post_image = PostImage.find(params[:id])
     post_image.destroy
@@ -21,7 +21,7 @@ class PostImagesController < ApplicationController
   end
 
   def index
-    @post_images = PostImage.all
+    @post_images = PostImage.page(params[:page])
   end
 
   def show
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @post_images = @user.post_images # 特定のユーザ（@user）に関連付けられた投稿全て（.post_images）を取得し@post_imagesに渡す という処理
+    @post_images = @user.post_images.page(params[:page]) s
   end
 
   def edit
